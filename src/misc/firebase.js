4b2d6b6aaf4564c0cc099c739366a50f48077b5f
sag_lyf <gosagnik@gmail.com>
2021-09-13 13:27:59 +0530

    initializes firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+    apiKey: 'AIzaSyCdsmWoV9mYdQLIG7-worST7jXLnD7FCpM',
+    authDomain: 'chat-web-app-1539f.firebaseapp.com',
+    projectId: 'chat-web-app-1539f',
+    storageBucket: 'chat-web-app-1539f.appspot.com',
+    messagingSenderId: '695068757314',
+    appId: '1:695068757314:web:52e56b37a374c190514cdb',
+};
+
+firebase.initializeApp(config);
