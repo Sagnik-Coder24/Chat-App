0367646256ff1fd3d6105bb5838263a3844bbf83
sag_lyf <gosagnik@gmail.com>
2021-09-16 01:37:10 +0530

    updated nickname inside the database

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,19 +1,34 @@
 import React from 'react';
-import { Drawer, Button, Divider } from 'rsuite';
+import { Drawer, Button, Divider, Alert } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
+import { database } from '../../misc/firebase';
 import EditableInput from '../EditableInput';
 
 const Dashboard = ({ onSignOut }) => {
     const { profile } = useProfile();
 
     const onSave = async newData => {
-        console.log(newData);
+        const userNicknameRef = database
+            .ref(`/profiles/${profile.uid}`)
+            .child('name');
+
+        try {
+            await userNicknameRef.set(newData);
+
+            Alert.success('Nickname has been updated', 4000);
+        } catch (err) {
+            Alert.error(err.message, 4000);
+        }
     };
 
     return (
         <>
             <Drawer.Header>
                 <Drawer.Title>Dashboard</Drawer.Title>
+            </Drawer.Header>
+
+            <Drawer.Body>
+                <h3>Hey, {profile.name}</h3>
                 <Divider />
                 <EditableInput
                     name="nickname"
@@ -21,10 +36,6 @@ const Dashboard = ({ onSignOut }) => {
                     onSave={onSave}
                     label={<h6 className="mb-2">Nickname</h6>}
                 />
-            </Drawer.Header>
-
-            <Drawer.Body>
-                <h3>Hey, {profile.name}</h3>
             </Drawer.Body>
 
             <Drawer.Footer>
