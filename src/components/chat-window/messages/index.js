09160f4446806f72f721b1bfbe5b0b8ef09673b5
sag_lyf <gosagnik@gmail.com>
2021-10-02 16:33:19 +0530

    handle message likes

--- src/components/chat-window/messages/index.js
+++ src/components/chat-window/messages/index.js
@@ -1,7 +1,8 @@
+/* eslint-disable no-param-reassign */
 import React, { useState, useEffect, useCallback } from 'react';
 import { useParams } from 'react-router';
 import { Alert } from 'rsuite';
-import { database } from '../../../misc/firebase';
+import { database, auth } from '../../../misc/firebase';
 import { transformToArrWithId } from '../../../misc/helper';
 import MessageItem from './MessageItem';
 
@@ -54,6 +55,36 @@ const Messages = () => {
         [chatId]
     );
 
+    const handleLike = useCallback(async msgId => {
+        const { uid } = auth.currentUser;
+        const messageRef = database.ref(`/messages/${msgId}`);
+
+        let alertMsg;
+
+        await messageRef.transaction(msg => {
+            if (msg) {
+                if (msg.likes && msg.likes[uid]) {
+                    msg.likeCount -= 1;
+                    msg.likes[uid] = null;
+                    alertMsg = 'Like removed';
+                } else {
+                    msg.likeCount += 1;
+
+                    if (!msg.likes) {
+                        msg.likes = {};
+                    }
+
+                    msg.likes[uid] = true;
+                    alertMsg = 'Like added';
+                }
+            }
+
+            return msg;
+        });
+
+        Alert.info(alertMsg, 4000);
+    }, []);
+
     return (
         <ul className="msg-list custom-scroll">
             {isChatEmpty && <li>No messages yet</li>}
@@ -63,6 +94,7 @@ const Messages = () => {
                         key={msg.id}
                         message={msg}
                         handleAdmin={handleAdmin}
+                        handleLike={handleLike}
                     />
                 ))}
         </ul>
