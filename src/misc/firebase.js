92ebca94cd5983fdfc4c0d5576b6309d31de0eeb
Sagnik Ghosh <83448857+Sagnik-Coder24@users.noreply.github.com>
2024-10-02 12:24:35 +0530

    Update firebase.js

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -4,12 +4,12 @@ import 'firebase/database';
 import 'firebase/storage';
 
 const config = {
-    apiKey: 'AIzaSyCdsmWoV9mYdQLIG7-worST7jXLnD7FCpM',
-    authDomain: 'chat-web-app-1539f.firebaseapp.com',
-    projectId: 'chat-web-app-1539f',
-    storageBucket: 'chat-web-app-1539f.appspot.com',
-    messagingSenderId: '695068757314',
-    appId: '1:695068757314:web:52e56b37a374c190514cdb',
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
 };
 
 const app = firebase.initializeApp(config);
